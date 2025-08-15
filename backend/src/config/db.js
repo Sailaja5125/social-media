7aa5a981ff331bf86775837b31a05725d48f4481
Sailaja5125 <sailajapuvala5125@gmail.com>
2025-08-16 04:23:53 +0530

    refactored the connect() method to async await in app.jsremoved clg in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { ENV } from './env.js';
 export const connect = async()=>{
     try {
-        console.log(ENV.MONGODB_URI)
+        
         await mongoose.connect(ENV.MONGODB_URI);
         console.log("Database connected successfully ✅🎉");
     } catch (error) {
