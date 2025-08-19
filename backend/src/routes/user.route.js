33db1cc0fd882acaa3614b1150bd49593b1f449b
Sailaja5125 <sailajapuvala5125@gmail.com>
2025-08-19 23:21:45 +0530

    did some backend modifications

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import { getUserProfile } from "../controllers/user.controller.js";
 const router = Router();
-
+router.get('/profile/:username',getUserProfile);
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connect } from './config/db.js';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/clerk-sdk-node';
+import userroutes from './routes/user.route.js';
 const app = express();
-
+app.use(cors()) 
+app.use(express.json()); // access JSON data in request body
+app.use(clerkMiddleware()); // Clerk middleware for authentication
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+// start with user routes 
+app.use('/api/users',userroutes)
 const startServer = async ()=>{
     try {
         await connect();
