0be36cbb14a4a3885b0804c7c179c8e6f6a93c46
Sailaja5125 <sailajapuvala5125@gmail.com>
2025-09-09 13:39:13 +0530

    changes in backend

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,18 +1,22 @@
-export const protectRoute =async(req , res , next)=>{
-        try{
-            if(!req.auth().isAuthenticated()){
-                return res.status(401).json({
-                    success: false,
-                    message: "Unauthorized access, please login first"
-                });
-            }
-            next();
-    
-        }catch(error){
-            res.status(500).json({
-                success: false,
-                message: "Internal server error",
-                error: error.message
-            });
-        }
+import { getAuth } from "@clerk/express"; // or "@clerk/nextjs/server" if you're using Next.js
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized access, please login first",
+      });
     }
+
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
