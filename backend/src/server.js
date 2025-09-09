c107eae9917ee4a9b428c2b8a1b2975b3b56fa5d
Sailaja5125 <sailajapuvala5125@gmail.com>
2025-09-09 13:27:37 +0530

    changes in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors()) 
 app.use(express.json()); // access JSON data in request body
 app.use(clerkMiddleware()); // Clerk middleware for authentication
-app.use(arcjetMiddleware); // arcjet middleware for security itself is an object 
+// app.use(arcjetMiddleware); // arcjet middleware for security itself is an object 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
